deefbff23d396030156d3d48797a3b93ba9ea327
RosanaNei <rosageene@gmail.com>
2022-08-24 23:45:30 -0300

    iniciando

---
+++ js/jScript.js
@@ -0,0 +1,15 @@
+//let button = getElementById('mostrar')
+let p = document.getElementById('whatever');
+/*function Mostrar(button){
+   addEventListener
+}*/
+
+
+p.removeAttribute("hidden");
+
+var a =document.getElementById('oculto');
+a.removeAttribute('hidden')
+
+// para comenzar, hacer foco el el primer elemento
+document.getElementsByClassName('tabis')[0].focus();
+
